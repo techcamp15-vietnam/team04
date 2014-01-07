2374cf447fc045e5fe04bed14616b4fe5317582b
zendbui <endlovez@gmail.com>
2014-01-07 18:42:07 +0700

    database connect

--- GrownMeUp/src/vn/techcamp/team04/grownmeup/MainMenuActivity.java
+++ GrownMeUp/src/vn/techcamp/team04/grownmeup/MainMenuActivity.java
@@ -6,17 +6,17 @@ import android.view.Menu;
 
 public class MainMenuActivity extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main_menu_screen);
-	}
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.main_menu_screen);
+        }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+        @Override
+        public boolean onCreateOptionsMenu(Menu menu) {
+                // Inflate the menu; this adds items to the action bar if it is present.
+                getMenuInflater().inflate(R.menu.main, menu);
+                return true;
+        }
 
 }
