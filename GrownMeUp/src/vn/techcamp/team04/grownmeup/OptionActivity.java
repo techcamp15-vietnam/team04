680230026426835d0a1bac9065c2be9e45264c12
Trung Hieu <trunghieu.hust@gmail.com>
2014-01-07 17:47:23 +0700

    Layout:option, add item, create custom stage, stage detail

--- GrownMeUp/src/vn/techcamp/team04/grownmeup/OptionActivity.java
+++ GrownMeUp/src/vn/techcamp/team04/grownmeup/OptionActivity.java
@@ -1,15 +1,27 @@
 package vn.techcamp.team04.grownmeup;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
 
-public class OptionActivity extends Activity {
+/**
+ * @author trung hieu
+ * 
+ */
+public class OptionActivity extends Activity implements OnClickListener {
+	private ImageButton btnAddItem;
+	private ImageButton btnCustomStage;
+	private ImageButton btnlanguage;
+	private ImageButton btnAbout;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.option_screen);
+		initView();
 	}
 
 	@Override
@@ -19,4 +31,40 @@ public class OptionActivity extends Activity {
 		return true;
 	}
 
+	/**
+	 * @init and set listener for button.
+	 * @param no
+	 * @return no
+	 */
+	private void initView() {
+		btnAddItem = (ImageButton) findViewById(R.id.option_add_item);
+		btnCustomStage = (ImageButton) findViewById(R.id.option_custom_stage);
+		btnlanguage = (ImageButton) findViewById(R.id.option_language);
+		btnAbout = (ImageButton) findViewById(R.id.option_about);
+
+		btnAddItem.setOnClickListener(this);
+		btnCustomStage.setOnClickListener(this);
+		btnlanguage.setOnClickListener(this);
+		btnAbout.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View view) {
+		switch (view.getId()) {
+		case R.id.option_add_item:
+
+			break;
+		case R.id.option_custom_stage:
+
+			break;
+		case R.id.option_language:
+
+			break;
+		case R.id.option_about:
+
+			break;
+		default:
+			break;
+		}
+	}
 }
